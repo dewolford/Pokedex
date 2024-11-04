5affbc3973b85842cdbc9b147ef810674f782064
Dani Wolford <dewolford42@tntech.edu>
2024-11-04 15:09:36 -0600

    making basic classes

--- BinaryTree.h
+++ BinaryTree.h
@@ -3,3 +3,47 @@
 // Purpose: hold the binary tree class
 //          and functions of said class
 //**************************************
+
+#ifndef BINARYTREE_H
+#define BINARYTREE_H
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include "Pokemon.h"
+
+using namespace std;
+
+class BinaryTree
+{
+    private:
+        struct TreeNode
+        {
+            Pokemon value;
+            TreeNode* leftPtr;
+            TreeNode* rightPtr;
+        };
+
+        TreeNode* rootPtr;
+
+        //functions
+        void insert(TreeNode);
+        void destroySubTree(TreeNode);
+        void displayInOrder(TreeNode);
+
+    public:
+        //constructors
+        BinaryTree();           //default
+        BinaryTree(TreeNode*);  //overloaded
+
+        //destructor
+        ~BinaryTree();
+
+        //other functions
+        void insertNode(TreeNode);
+        void displayInOrder();
+        bool searchNode(TreeNode);
+
+};
+
+#endif
--- Pokemon.h
+++ Pokemon.h
@@ -2,3 +2,39 @@
 // Title:   Pokemon.h
 // Purpose: hold the pokemon class and functions
 //****************************************************
+
+#ifndef POKEMON_H
+#define POKEMON_H
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include "BinaryTree.h"
+
+using namespace std;
+
+class Pokemon
+{
+    private:
+        int indexNum;
+        string Name;
+    public:
+        //constructors
+        Pokemon();              //default
+        Pokemon(int, string);   //overloaded
+
+        //setters
+        void setID(int);
+        void setName(string);
+
+        //getters
+        int getID();
+        string getName();
+
+        //operators
+        /*   < operator     */
+        /*   == operator    */
+        /*   << operator    */
+};
+
+#endif
